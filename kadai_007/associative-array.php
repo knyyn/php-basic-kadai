806891d4fe89bd78cc60ba470ab05a68af36368a
knyyn <azukinako02151@gmail.com>
2025-02-06 00:58:57 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>課題７ 連想配列を作ってキーと値を出力しよう</title>
+</head>
+
+<body>
+   <p>
+      <?php
+      //連想配列に値を代入する
+      $goods_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      //値を出力する
+      print_r($goods_data);
+      ?>
+  </p>
+<body>
+
+</html>
